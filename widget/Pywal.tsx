c1a1c5f86933c584b700a1663c8a8b59652f01e5
Benjamin Palko <benjaminpalko@hotmail.com>
2025-04-04 09:49:21 -0400

    create pywal widget

---
+++ widget/Pywal.tsx
@@ -0,0 +1,17 @@
+import { exec } from "astal";
+import { Gdk } from "astal/gtk4";
+
+function Pywal() {
+  return (
+    <box>
+      <button
+        cssClasses={["Pywal"]}
+        cursor={Gdk.Cursor.new_from_name("pointer", null)}
+        label={""}
+        onClicked={() => exec("bash -c ~/dotfiles/.scripts/pywal-swww.sh")}
+      />
+    </box>
+  );
+}
+
+export default Pywal;
