9f2db8d7e91e8548d9c6d5273d2af7f3222d7d23
Benjamin Palko <benjaminpalko@hotmail.com>
2025-04-04 09:49:27 -0400

    internet widget

---
+++ widget/Internet.tsx
@@ -0,0 +1,36 @@
+import { bind } from "astal";
+import Network from "gi://AstalNetwork";
+
+function Internet() {
+  const network = Network.get_default();
+
+  const icon = (strength: number) => {
+    if (strength > 80) {
+      return "󰤨";
+    }
+    if (strength > 60) {
+      return "󰤥";
+    }
+    if (strength > 40) {
+      return "󰤢";
+    }
+    if (strength > 20) {
+      return "󰤟";
+    }
+    return "󰤯";
+  };
+
+  return (
+    <box>
+      <label
+        label={bind(network, "wifi").as(
+          (w) => `${icon(w.strength)}  ${w.strength}%`,
+        )}
+        hasTooltip={true}
+        tooltip_text={bind(network, "wifi").as((w) => w.ssid)}
+      />
+    </box>
+  );
+}
+
+export default Internet;
