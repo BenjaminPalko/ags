ecc664bcaf5070b4158286d2c593fd7714048a6f
Benjamin Palko <benjaminpalko@hotmail.com>
2025-04-08 00:15:34 -0400

    use bindings

--- widget/Tray.tsx
+++ widget/Tray.tsx
@@ -1,15 +1,11 @@
 import { bind } from "astal";
-import { Gdk, Gtk } from "astal/gtk4";
+import { Gdk } from "astal/gtk4";
 import AstalTray from "gi://AstalTray";
 
 const Tray = function () {
   const tray = AstalTray.get_default();
 
   const trayItems = bind(tray, "items");
-
-  function OpenTrayItem(button: Gtk.MenuButton, item: AstalTray.TrayItem) {
-    item.about_to_show();
-  }
 
   return (
     <box cssClasses={["Tray"]}>
@@ -18,8 +14,10 @@ const Tray = function () {
           return (
             <menubutton cursor={Gdk.Cursor.new_from_name("pointer", null)}>
               <image
-                tooltip_text={item.title || item.tooltip.title}
-                file={item.iconName || "NONE"}
+                tooltip_text={bind(item, "tooltip_markup")}
+                file={bind(item, "icon_name").as(
+                  (iconName) => iconName || "NONE",
+                )}
               />
             </menubutton>
           );
