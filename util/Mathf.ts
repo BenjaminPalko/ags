23f28f03fe9589effb0a6a5f6530f9445eccfa05
Benjamin Palko <benjaminpalko@hotmail.com>
2025-06-13 17:21:50 -0400

    quick maf

--- util/Mathf.ts
+++ util/Mathf.ts
@@ -1,12 +1,12 @@
 export const Mathf = {
-  clamp: (value: number, min: number, max: number) => {
-    if (value < min) return min;
-    if (value > max) return max;
-    return value;
-  },
-  sign: (value: number) => {
-    if (value > 0) return 1;
-    if (value < 0) return -1;
-    return 0;
-  },
+	clamp: (value: number, min: number, max: number) => {
+		if (value < min) return min;
+		if (value > max) return max;
+		return value;
+	},
+	sign: (value: number) => {
+		if (value > 0) return 1;
+		if (value < 0) return -1;
+		return 0;
+	},
 };
