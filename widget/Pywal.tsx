28141a5bc3b6ec444b69cc31946c3b0c891512f6
Benjamin Palko <benjaminpalko@hotmail.com>
2025-06-23 08:54:01 -0400

    idk formatting i guess

--- widget/Pywal.tsx
+++ widget/Pywal.tsx
@@ -2,14 +2,14 @@ import { exec } from "astal";
 import { Gdk } from "astal/gtk4";
 
 function Pywal() {
-  return (
-    <button
-      cssClasses={["Pywal"]}
-      cursor={Gdk.Cursor.new_from_name("pointer", null)}
-      label={""}
-      onClicked={() => exec("bash -c ~/dotfiles/.scripts/pywal-swww.sh")}
-    />
-  );
+	return (
+		<button
+			cssClasses={["Pywal", "Button"]}
+			cursor={Gdk.Cursor.new_from_name("pointer", null)}
+			label={""}
+			onClicked={() => exec("bash -c ~/dotfiles/.scripts/pywal-swww.sh")}
+		/>
+	);
 }
 
 export default Pywal;
