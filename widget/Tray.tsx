7426b15fb722eea9d727e83c582ee3db174cee4b
Benjamin Palko <benjaminpalko@hotmail.com>
2025-06-23 08:54:18 -0400

    tray icons fixed

--- widget/Tray.tsx
+++ widget/Tray.tsx
@@ -5,13 +5,15 @@ import AstalTray from "gi://AstalTray";
 const TrayItem = function ({ item }: { item: AstalTray.TrayItem }) {
 	const popover = Gtk.PopoverMenu.new_from_model(item.menu_model);
 	popover.insert_action_group("dbusmenu", item.action_group);
+	popover.add_css_class("TrayItem");
 
 	return (
 		<menubutton
 			cursor={Gdk.Cursor.new_from_name("pointer", null)}
-			icon_name={item.icon_name}
 			popover={popover}
-		/>
+		>
+			<image gicon={item.gicon} cssClasses={["Label"]} />
+		</menubutton>
 	);
 };
 
